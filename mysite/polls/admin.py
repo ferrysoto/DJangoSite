8085b67c63a4c9a5e06c655a5c6c7383fa50a90a
Ferran Herrero <ferran.herrerosoto@gmail.com>
2019-10-07 17:58:15 +0200

    It Works

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -11,10 +11,8 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-         list_display = ('question_text', 'pub_date', 'was_published_recently'),
     ]
     inlines = [AnswerInline]
-    list_filter = ['pub_date']
 
 admin.site.register(Answers)
 admin.site.register(Question, QuestionAdmin)
